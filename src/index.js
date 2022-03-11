6507b4607342f56258622dbc3f8d6a167c5845a3
Mohammad hossein <mohammadhossein@Mohammads-MacBook-Air.local>
2022-03-11 18:00:21 +0330

    add more shet

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+import App from "./App.js";
 import "./index.css"
 
 ReactDOM.render(
