d95100d76420f788c32aa397a3ed9606867e9f48
Mohammad hossein <mohammadhossein@Mohammads-MacBook-Air.local>
2022-03-12 14:02:07 +0330

    add more function

--- src/helper/functions.js
+++ src/helper/functions.js
@@ -1,14 +1,17 @@
+// add function one
 const shorten = (title) => {
     const splitedTitle = title.split(" ");
     const newTitle = `${splitedTitle[0]} ${splitedTitle[1]}`
     return newTitle;
 }
 
+// add function two
 const isInCart = (state, id) => {
     const result = !!state.selectedItems.find(item => item.id === id)
     return result;
 }
 
+// add function three
 const quantityCount = (state, id) => {
     const index = state.selectedItems.findIndex(item => item.id === id);
     if (index === -1) {
